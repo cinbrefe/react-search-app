0b7df9bace2af432268c57794bc868ab4c99bf5f
Cindy Brenes <cindybrenes@gmail.com>
2026-06-17 11:49:04 -0500

    Add eslint-plugin-jsx-a11y and eslint-plugin-react

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,17 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import reactPlugin from 'eslint-plugin-react'
 
 export default defineConfig([
 	globalIgnores(['dist']),
 	{
-		files: ['**/*.{js,jsx}'],
+		files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+		plugins: {
+			'jsx-a11y': jsxA11y,
+			'react': reactPlugin,
+		},
 		extends: [
 			js.configs.recommended,
 			reactHooks.configs.flat.recommended,
@@ -19,6 +25,9 @@ export default defineConfig([
 		},
 		rules: {
 			semi: ['error', 'never'],
+			...jsxA11y.flatConfigs.recommended.rules,
+
+			'jsx-a11y/anchor-is-valid': 'warn',
 		},
 	},
 ])
